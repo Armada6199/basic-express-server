ca9e48adf8d64178b50ad447af454c7db579e98b
=armada6199 <moh.abdin27@gmail.com>
2023-06-14 22:44:42 +0300

    adding home route

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ const app=express();
 const pageNotFound=require('./error-handlers/404');
 const internalServarError=require('./error-handlers/500');
 const nameValidator=require('./middleware/validator');
+app.get('/',(req,res,next)=>{
+    res.send('you are on the home page try using /preson with name ')
+})
 app.get('/person',nameValidator,(req,res,next)=>{
 })
 app.use('*',pageNotFound);
